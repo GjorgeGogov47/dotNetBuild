3a28ae631aeab076cee6606cd2807c8b15de3d8e
Gjorge Gogov <gjorge.gogov@hotmail.com>
2024-05-17 23:18:06 +0200

    improved design with Material UI

--- 016. React/PizzaClient/src/main.jsx
+++ 016. React/PizzaClient/src/main.jsx
@@ -3,10 +3,17 @@ import ReactDOM from 'react-dom/client'
 //import App from './App.jsx'
 //import './index.css'
 
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+const theme = createTheme();
+
 import Pizza from './Pizza'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Pizza />
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Pizza className="Pizza"/>
+    </ThemeProvider>
   </React.StrictMode>,
 )
